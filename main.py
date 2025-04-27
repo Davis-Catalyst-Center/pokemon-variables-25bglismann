84488de39d99fd5f70cb1e1ae599841ef30571e3
25bglismann <25bglismann@go.dsdmail.net>
2025-04-27 16:43:14 +0000

    information about my pokemon

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "Squirtle"
+print("My Pokemon's name is", name)
+category = "tiny turtle"
+print("My Pokemon's category is", category)
+gender = "male or female"
+print("My Pokemon gender is", gender)
+height = 50.8 # in cm
+print("My Pokemon's height is", height, "cm")
+weight = 19.8
+print("My Pokemon's weight is", weight)
+type = "water"
+print("My Pokemon's type is", type)
+weakness = " grass and electric"
+print("My Pokemon's weakness is", weakness)
+abilities = "torrent"
+print("My Pokemon's abilities are", abilities)
